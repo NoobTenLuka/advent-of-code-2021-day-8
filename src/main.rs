d5601613610af2d620616da101a4f282fa1159fc
NTLuka <noobtenluka@gmail.com>
2021-12-08 12:39:23 +0100

    changed datatype of inputs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::{path::Path, fs};
 fn main() {
     let inputs = read_inputs("input.txt");
 
-    solution_1(&inputs.1);
+    solution_1(&inputs);
 }
 
-fn solution_1(output_values: &Vec<String>) {
+fn solution_1(output_values: &Vec<(String, String)>) {
     let num = output_values.iter().fold(0, |upper_acc, o| {
-        upper_acc + o.split(' ').fold(0, |acc, e| {
+        upper_acc + o.1.split(' ').fold(0, |acc, e| {
             match e.len() {
                 2 | 4 | 7 | 3 => {
                     acc + 1
@@ -21,20 +21,14 @@ fn solution_1(output_values: &Vec<String>) {
     println!("The number of unique digits is {}", &num);
 }
 
-fn read_inputs<T: AsRef<Path>>(path: T) -> (Vec<String>, Vec<String>) {
+fn read_inputs<T: AsRef<Path>>(path: T) -> Vec<(String, String)> {
     let file_content = fs::read_to_string(path).expect("Input file not found");
 
-    let mut input_vec: Vec<String> = vec![];
-    let mut output_vec: Vec<String> = vec![];
-
-    file_content.split('\n').for_each(|e| {
+    file_content.split('\n').map(|e| {
         let input = e.split_at(59);
 
         let output = input.1.replace('|', "");
 
-        input_vec.push(input.0.trim().to_string());
-        output_vec.push(output.trim().to_string());
-    });
-
-    (input_vec, output_vec)
+        (input.0.trim().to_string(), output.trim().to_string())
+    }).collect()
 }
